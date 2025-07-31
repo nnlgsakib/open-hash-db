2bef287f22c8bbf3e0c9044d838512adea8a90c4
nnlgsakib <nlgarts@gmail.com>
2025-07-31 20:12:59 +0600

    rmvd node

--- network/bootnode/bootnode.go
+++ network/bootnode/bootnode.go
@@ -237,7 +237,7 @@ type bootnodesWrapper struct {
 
 // DefaultBootnodes for the VPS-hosted default node
 var DefaultBootnodes = []string{
-	"/ip4/148.251.35.204/tcp/9090/p2p/QmYQMdZkvC4R7DHqSkCKNh89Hs7gDPLjx9j9xnPsUkkS2P",
+	// "/ip4/148.251.35.204/tcp/9090/p2p/QmYQMdZkvC4R7DHqSkCKNh89Hs7gDPLjx9j9xnPsUkkS2P",
 }
 
 // loadOrCreateIdentity loads or creates a private key
