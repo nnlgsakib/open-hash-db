4fff11298bea588a86f3eb4439b827c462d57228
nnlgsakib <nlgarts@gmail.com>
2025-08-02 10:26:06 +0600

    seprated the bootnodes forr readability

--- network/libp2p/node.go
+++ network/libp2p/node.go
@@ -186,11 +186,6 @@ func loadOrCreateIdentity(keyPath string) (crypto.PrivKey, error) {
 	return privKey, nil
 }
 
-// DefaultBootnodes for the VPS-hosted default node
-var DefaultBootnodes = []string{
-	// "/ip4/148.251.35.204/tcp/9090/p2p/QmYQMdZkvC4R7DHqSkCKNh89Hs7gDPLjx9j9xnPsUkkS2P",
-}
-
 // convertBootnodesToAddrInfo converts multiaddresses to peer.AddrInfo
 func convertBootnodesToAddrInfo(bootnodes []string) ([]peer.AddrInfo, error) {
 	var addrInfos []peer.AddrInfo
