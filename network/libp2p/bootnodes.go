11bdab8264523c5f5013616da202eff33da4f5d0
nnlgsakib <nlgarts@gmail.com>
2025-09-13 15:44:17 +0600

    fix: Update DefaultBootnodes with correct peer ID

--- network/libp2p/bootnodes.go
+++ network/libp2p/bootnodes.go
@@ -1,7 +1,7 @@
 package libp2p
 
 var DefaultBootnodes = []string{
-	"/ip4/148.251.35.204/udp/9090/quic-v1/p2p/QmZZqKCPP1yeuJ5ektEfbkn5ToEUuXYoJudV2KtqYWuDan",
+	"/ip4/148.251.35.204/udp/9090/quic-v1/p2p/QmZsokEATEZJQ7Ua2w2uBFNjrCd2d4pjAWgD6MQ7HzPNB6",
 	// "/dnsaddr/sg1.bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
 	// "/dnsaddr/sv15.bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
 	// "/dnsaddr/am6.bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
